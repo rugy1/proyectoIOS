eeecfddea7ed19c6c278dcd358789546cb0bc276
Rogelio Escalante <Rogelio@MBP-de-Rogelio.lan>
2015-10-19 08:41:25 -0500

    Initial Commit

---
+++ Asistencia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Asistencia
+//
+//  Created by Rogelio Escalante on 19/10/15.
+//  Copyright (c) 2015 Rogelio Escalante. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Asistencia/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Asistencia
+//
+//  Created by Rogelio Escalante on 19/10/15.
+//  Copyright (c) 2015 Rogelio Escalante. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
